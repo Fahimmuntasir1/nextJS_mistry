3a9e26dce8de0df8763b611932216d310d107710
Fahimmuntasir1 <fahimmuntasir043@gmail.com>
2024-05-25 14:09:54 +0600

    blogs and nested rout working

---
+++ app/blogs/English/page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const english = () => {
+    return (
+        <div>
+            <h1>This is english blog page</h1>
+        </div>
+    );
+};
+
+export default english;
---
+++ app/blogs/page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const blogPage = () => {
+    return (
+        <div>
+            <h1 className='text-center'>This is Blog Page.</h1>
+        </div>
+    );
+};
+
+export default blogPage;
