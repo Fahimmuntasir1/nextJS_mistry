f9f4f1408871cbb3bf08b18e86704cc9b1ccbe56
Fahimmuntasir1 <fahimmuntasir043@gmail.com>
2024-06-16 20:31:37 +0600

    not found page updated

--- app/not-found.js
+++ app/not-found.js
@@ -3,8 +3,8 @@ import React from 'react';
 const notfoundPage = () => {
     return (
         <div>
-            <h2>404 requested page not found!</h2>
-        </div>
+            <h2 className='text-center text-3xl flex justify-center items-center '>404 requested page not found!</h2>
+        </div >
     );
 };
 
