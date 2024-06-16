bfdc9e24695235242c4ef80366ca16dea0695bf6
Fahimmuntasir1 <fahimmuntasir043@gmail.com>
2024-06-16 20:08:25 +0600

    not found page created

---
+++ app/not-found.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const notfoundPage = () => {
+    return (
+        <div>
+            <h2>404 requested page not found!</h2>
+        </div>
+    );
+};
+
+export default notfoundPage;
