dbdec1874623a8cdff5970613f6d7a6f27b2d19f
Fahimmuntasir1 <fahimmuntasir043@gmail.com>
2024-06-21 16:59:44 +0600

    error page and handeling

--- app/about/page.js
+++ app/about/page.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 const aboutPage = () => {
+    // throw new error('There are an Error in this application')
     return (
         <div>
             <h1>This is about page</h1>
