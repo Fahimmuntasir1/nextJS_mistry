1b439a8406891ff2871e07ce1372e5097b701726
Fahimmuntasir1 <fahimmuntasir043@gmail.com>
2024-06-21 23:00:22 +0600

    app updated

--- app/blogs/international/page.js
+++ app/blogs/international/page.js
@@ -1,11 +1,23 @@
 import React from 'react';
 
 const international = () => {
+    const blogs = [
+        { "title": "First Object", "description": "This is the first object." },
+        { "title": "Second Object", "description": "This is the second object." },
+
+    ]
     return (
         <div>
             <h1>
                 This is International page.
             </h1>
+            <ul>
+                {
+                    blogs.map(blog =>
+                        <li key={blog.title}>{blog.title}</li>
+                    )
+                }
+            </ul>
         </div>
     );
 };
